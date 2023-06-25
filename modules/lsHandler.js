42348c2cfff2f5e2a9d395b17a3ef4e47b5b740f
agtugchik <agtugchik13@gmail.com>
2023-06-25 10:46:46 +0300

    refacor: ls handler

--- modules/lsHandler.js
+++ modules/lsHandler.js
@@ -1,27 +1,26 @@
 import { readdir, stat } from "fs/promises";
 import { join } from "path";
 
-const getFileType = async (currentPath, v) => {
-  let type = "file";
-  try {
-    type = (await stat(join(currentPath, v))).isFile() ? "file" : "directory";
-  } catch {}
-  return type;
-};
-
 const lsHandler = async (currentPath) => {
-  const tableStructure = await Promise.all(
-    (
-      await readdir(currentPath)
-    ).map(async (v) => {
-      return {
-        Name: v,
-        Type: await getFileType(currentPath, v),
-      };
-    })
-  );
-  console.table(tableStructure);
-  console.log(`You are currently in ${currentPath}`);
+  try {
+    const tableStructure = await Promise.all(
+      (
+        await readdir(currentPath)
+      ).map(async (v) => {
+        return {
+          Name: v,
+          Type: (await stat(join(currentPath, v))).isFile()
+            ? "file"
+            : "directory",
+        };
+      })
+    );
+    console.table(tableStructure);
+    console.log(`You are currently in ${currentPath}`);
+  } catch {
+    console.log("Operation failed");
+    console.log(`You are currently in ${currentPath}`);
+  }
 };
 
 export default lsHandler;
